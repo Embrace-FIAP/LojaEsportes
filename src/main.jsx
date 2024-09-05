964b868eb12fac00fa1b659c6c3071be75e3c24b
Guilherme <joseguilhermecosta@gmail.com>
2024-09-04 21:03:34 -0300

    Adicionando a estrutura base do projeto

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
-import './index.css'
+import Error from './routes/Error.jsx'
+import Home from './routes/Home.jsx'
+import Produtos from './routes/Produtos.jsx'
+import Sobre from './routes/Sobre.jsx'
+import Contato from './routes/Contato.jsx'
+
+const router = createBrowserRouter([
+  {
+    // Elementos pai
+    path: '/', element: <App />,
+    errorElement: <Error />,
+    
+    // Elementos filho
+    children: [
+      {path: '/', element: <Home />},
+      {path: '/produtos', element: <Produtos />},
+      {path: '/sobre', element: <Sobre />},
+      {path: '/contato', element: <Contato />}
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* Criando a desestruturação para que seja renderizada */}
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/routes/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error = () => {
+    return (
+        <>
+            <h1>Erro 404 - Página não Encontrada</h1>
+        </>
+    )
+}
+
+export default Error

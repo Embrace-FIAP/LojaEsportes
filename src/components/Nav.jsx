3e40ec0bcf0bbd47e61077ad89f7b20ba45cd391
Guilherme <joseguilhermecosta@gmail.com>
2024-09-05 09:54:53 -0300

    Finalizando a barra de navegação

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,13 +2,16 @@ import {Link} from 'react-router-dom'
 import React from 'react'
 import '../css/Base.css'
 import '../css/Nav.css'
+import Trofeu from '../assets/trofeu.png'
 
 const Nav = () => {
     return (
         <div className='navBar'>
-            <Link to='/'>Home</Link>
-            <Link to='/produtos'>Produtos</Link>
-            <Link to='/contato'>Contato</Link>
+            <img src={Trofeu} className='logoImg' />
+            <h1 className='logo'>Champions</h1>
+            <Link to='/' className='navLink'>Home</Link>
+            <Link to='/produtos' className='navLink'>Produtos</Link>
+            <Link to='/contato' className='navLink'>Contato</Link>
         </div>
     )
 }
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,9 +4,9 @@ import '../css/Home.css'
 
 const Home = () => {
     return (
-        <>
+        <div className='home'>
             <h1>Home</h1>
-        </>
+        </div>
     )
 }
 

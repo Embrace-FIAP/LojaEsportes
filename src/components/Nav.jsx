22d6818f28b2d834318a926d7bd907230fa6652a
Guilherme <joseguilhermecosta@gmail.com>
2024-09-05 09:25:32 -0300

    Avançando no css

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,15 @@
 import {Link} from 'react-router-dom'
 import React from 'react'
+import '../css/Base.css'
+import '../css/Nav.css'
 
 const Nav = () => {
     return (
-        <>
+        <div className='navBar'>
             <Link to='/'>Home</Link>
             <Link to='/produtos'>Produtos</Link>
             <Link to='/contato'>Contato</Link>
-        </>
+        </div>
     )
 }
 
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import '../css/Base.css'
+import '../css/Home.css'
 
 const Home = () => {
     return (

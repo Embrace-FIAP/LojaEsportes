fe088c1cee250847e826988744c75c140358375f
Fernando Navajas Moraes <fernandonm07@hotmail.com>
2024-09-05 09:03:26 -0300

    Link funcionais

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,12 @@
+import {Link} from 'react-router-dom'
 import React from 'react'
 
 const Nav = () => {
     return (
         <>
-      
+            <Link to='/'>Home</Link>
+            <Link to='/produtos'>Produtos</Link>
+            <Link to='/contato'>Contato</Link>
         </>
     )
 }

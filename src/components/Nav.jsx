d9813d5c7f47ba5e16ce0a1c60b341512236de79
Guilherme <joseguilhermecosta@gmail.com>
2024-09-05 16:23:07 -0300

    Ajustes finais do Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,16 +2,26 @@ import {Link} from 'react-router-dom'
 import React from 'react'
 import '../css/Base.css'
 import '../css/Nav.css'
-import Trofeu from '../assets/trofeu.png'
+import Trofeu from '../assets/trofeupreto.png'
+import Lupa from '../assets/lupa.png'
+import Coracao from '../assets/coracao.png'
+import Carrinho from '../assets/carrinho.png'
 
 const Nav = () => {
     return (
         <div className='navBar'>
             <img src={Trofeu} className='logoImg' />
-            <h1 className='logo'>Champions</h1>
             <Link to='/' className='navLink'>Home</Link>
             <Link to='/produtos' className='navLink'>Produtos</Link>
             <Link to='/contato' className='navLink'>Contato</Link>
+            <div className='searchBox'>
+                <img src={Lupa} className='searchImg' />
+                <input type='text' placeholder='Buscar' className='searchInput' />
+            </div>
+            <div className='navBuy'>
+                <img src={Coracao} className='coracaoImg' />
+                <img src={Carrinho} className='carrinhoImg' />
+            </div>
         </div>
     )
 }

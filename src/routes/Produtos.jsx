408fdded68d04d552b54ec9f12a755b931cdb87d
Guilherme <joseguilhermecosta@gmail.com>
2024-09-06 19:14:47 -0300

    Iniciando a página Produtos

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,10 +1,55 @@
 import React from 'react'
+import '../css/Produtos.css'
+import { Link } from 'react-router-dom'
 
 const Produtos = () => {
     return (
-        <>
-            <h1>Produtos</h1>
-        </>
+        <div className='produtos'>
+
+            <h1 className='sectionHeading'>Produtos</h1>
+
+            <div className='cardSection'>
+                <div className='card'>
+                    <p className='cardTitle'>Título do produto</p>
+                    <p className='cardSubtitle'>Corrida</p>
+                    <p className='cardPrice'>R$832,00</p>
+                </div>
+                <div className='card'>
+                    <p className='cardTitle'>Título do produto</p>
+                    <p className='cardSubtitle'>Corrida</p>
+                    <p className='cardPrice'>R$832,00</p>
+                </div>
+                <div className='card'>
+                    <p className='cardTitle'>Título do produto</p>
+                    <p className='cardSubtitle'>Corrida</p>
+                    <p className='cardPrice'>R$832,00</p>
+                </div>
+            </div>
+
+            <div className='cardSection'>
+                <Link to='/sobre'>
+                    <div className='card2'>
+                        <p className='cardTitle2'>Título do produto</p>
+                        <p className='cardSubtitle2'>Boxing</p>
+                        <p className='cardPrice2'>R$832,00</p>
+                    </div>
+                </Link>
+                
+                <div className='card2'>
+                    <p className='cardTitle2'>Título do produto</p>
+                    <p className='cardSubtitle2'>Boxing</p>
+                    <p className='cardPrice2'>R$832,00</p>
+                </div>
+                <div className='card2'>
+                    <p className='cardTitle2'>Título do produto</p>
+                    <p className='cardSubtitle2'>Boxing</p>
+                    <p className='cardPrice2'>R$832,00</p>
+                </div>
+            </div>
+
+            <br></br>
+
+        </div>
     )
 }
 

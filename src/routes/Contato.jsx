abe1f1563207bd170cb2ee08d7a1eb990f480e6c
Guilherme <joseguilhermecosta@gmail.com>
2024-09-08 17:18:39 -0300

    Finalizando a página Contato

--- src/routes/Contato.jsx
+++ src/routes/Contato.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import '../css/Contato.css'
 
 const Contato = () => {
     return (
-        <>
-            <h1>Contato</h1>
-        </>
+        <div className='contato'>
+            <div className='contatoSection'>
+                <h3 className='heading3'>Entre em contato conosco</h3>
+                <h1 className='heading1'>Contato WIN</h1>
+                <p className='text'>
+                Nossa equipe é formada por especialistas apaixonados por esporte, comprometidos em ajudar você a escolher os melhores produtos para cada objetivo. Aqui, você não apenas encontra equipamentos, mas também orientação profissional para elevar seu desempenho.
+                </p>
+                <p className='text'><span>Telefone:</span> 98764-2742</p>
+                <p className='text'><span>Instagram:</span> winsports</p>
+                <h3 className='heading3'>
+                Seja parte da nossa comunidade de vencedores!
+                </h3>
+            </div>
+        </div>
     )
 }
 

3157cb202a873cbb97fee4648a0d9b288e917a2f
Guilherme <joseguilhermecosta@gmail.com>
2024-09-09 17:42:27 -0300

    Finalizando o projeto

--- src/routes/Contato.jsx
+++ src/routes/Contato.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import '../css/Contato.css'
+import ContatoImg from '../assets/contatoimg.avif'
+import Youtube from '../assets/youtube.png'
+import Instagram from '../assets/instagram.png'
+import WhatsApp from '../assets/whatsapp.png'
 
 const Contato = () => {
     return (
@@ -10,11 +14,13 @@ const Contato = () => {
                 <p className='text'>
                     Nossa equipe é formada por especialistas apaixonados por esporte, comprometidos em ajudar você a escolher os melhores produtos para cada objetivo. Aqui, você não apenas encontra equipamentos, mas também orientação profissional para elevar seu desempenho.
                 </p>
-                <p className='text'><span>Telefone:</span> 98764-2742</p>
-                <p className='text'><span>Instagram:</span> winsports</p>
+                <img src={ContatoImg} className='contatoImg' />
                 <h3 className='heading3'>
                     Seja parte da nossa comunidade de vencedores!
                 </h3>
+                <p className='text2'><img src={Instagram} className='instagramImg' /><span>Instagram:</span> winsports</p>
+                <p className='text2'><img src={Youtube} className='youtubeImg' /><span>Youtube: </span> WIN Sports</p>
+                <p className='text3'><img src={WhatsApp} className='whatsappImg' /><span>WhatsApp:</span> 21 98764-2742</p>
             </div>
         </div>
     )
--- src/routes/Sobre.jsx
+++ src/routes/Sobre.jsx
@@ -9,10 +9,10 @@ const Sobre = () => {
                 <h3 className='sobreHeading3'>Conheça um pouco mais a equipe WIN</h3>
                 <h1 className='sobreHeading1'>Nossa História</h1>
                 <img src={SobreImg} className='sobreImg' />
-                <p className='text'>
+                <p className='sobreText'>
                     Fundada por <span>apaixonados</span> por esportes, a <span>WIN</span> nasceu com o objetivo de alavancar atletas de todos os níveis. Desde nossa criação, temos trabalhado para oferecer produtos que combinam <span>inovação</span>, <span>desempenho</span> e <span>estilo</span>. Inspirados pelo espírito competitivo, nossa logo em forma de troféu simboliza a vitória e o compromisso em <span>alcançar sempre o topo</span>.
                 </p>
-                <p className='text2'>
+                <p className='sobreText2'>
                     Na <span>WIN</span>, acreditamos que cada atleta carrega dentro de si a chama da vitória. Nossa missão é oferecer produtos de alta qualidade que ajudam você a alcançar o seu melhor, seja nas pistas, nas quadras ou nas academias. Aqui, cada peça de equipamento é projetada para <span>transformar desafios em conquistas</span>.
                 </p>
             </div>
